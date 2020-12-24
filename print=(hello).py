2fc2814f0987e480ac673fb1d7e31b3d3733d1e6
nimiko24 <68658629+nimiko24@users.noreply.github.com>
2020-12-23 22:12:10 -0500

    New image

--- print=(hello).py
+++ print=(hello).py
@@ -1,12 +1,17 @@
-
 print ("Hola, ¿Cuál es tu nombre?")
 Now = input()
 print("Un gusto " + Now )
 print("¿Cuántos años tienes?")
 myAge = input()
 mouse = str(int(myAge) + int(10))
-Lets = 11
 print('Si a tu edad le sumamos 10 y le multiplicamos por 11 tendrás ' + str(int(mouse) * int(11)) + ' años.')
-print("10**2 = " + str(int(10)) ** int(2))
-print()
+print("10**2 = " + str(int(10) ** int(2))
 #¿Continuamos?
+x = input()
+z = int(x)**(2)
+y = int(2) - int(2)*int(x)
+if z < y:
+  print("red")
+
+else:
+  print("Green")
