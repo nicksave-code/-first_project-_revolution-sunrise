618e463070d50affcf034cc0b26711c84f4e073f
nimiko24 <68658629+nimiko24@users.noreply.github.com>
2021-01-04 19:54:14 -0500

    More Life for Computer and I

--- Code/firstcode.cpp
+++ Code/firstcode.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 #include <cstdlib>
-#include <Windows.h>
 using namespace std;
 
 int main()
 {
     std::string test = u8"Hola Sandra\n";
-       SetConsoleOutputCP(CP_UTF8);
        std::cout << test;
     system("pause");
     return 0;
